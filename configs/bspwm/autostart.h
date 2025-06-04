56cc6ce40e56fae7acbde83307db86eb73ef729d
Jayasurya-Marasani <jayasuryamarasani@gmail.com>
2025-06-04 13:50:00 +0530

    Add bspwm sxhkd

---
+++ configs/bspwm/autostart.h
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+function run {
+  if ! pgrep $1 ;
+  then
+    $@&
+  fi
+}
+
+#Find out your monitor name with xrandr or arandr (save and you get this line)
+#xrandr --output VGA-1 --primary --mode 1360x768 --pos 0x0 --rotate normal
+#xrandr --output DP2 --primary --mode 1920x1080 --rate 60.00 --output LVDS1 --off &
+#xrandr --output LVDS1 --mode 1366x768 --output DP3 --mode 1920x1080 --right-of LVDS1
+#xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
+#autorandr horizontal
+
+
+#change your keyboard if you need it
+#setxkbmap -layout be
+
+keybLayout=$(setxkbmap -v | awk -F "+" '/symbols/ {print $2}')
+
+if [ $keybLayout = "be" ]; then
+  run sxhkd -c ~/.config/bspwm/sxhkd/sxhkdrc-azerty &
+else
+  run sxhkd -c ~/.config/sxhkd/sxhkdrc &
+fi
+
+#Some ways to set your wallpaper besides variety or nitrogen
+#feh --bg-scale ~/.config/bspwm/wall.png &
+feh --bg-fill /usr/share/backgrounds/archlinux/arch-wallpaper.jpg &
+feh --bg-fill /usr/share/backgrounds/arco/arco-wallpaper.jpg &
+#wallpaper for other Arch based systems
+#feh --bg-fill /usr/share/archlinux-tweak-tool/data/wallpaper/wallpaper.png &
+#feh --randomize --bg-fill ~/Dropbox/Apps/Desktoppr/*
+
+xsetroot -cursor_name left_ptr &
+
+run variety &
+run nm-applet &
+run pamac-tray &
+numlockx on &
